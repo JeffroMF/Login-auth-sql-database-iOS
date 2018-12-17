6cf3832da9cb9172b68e0c9e3c3994f8d18c016a
Bruno <bruno.mcamps@outlook.com>
2018-12-17 14:38:09 -0200

    Initial Commit

---
+++ Trybes v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trybes v2
+//
+//  Created by Bruno Campos on 12/17/18.
+//  Copyright © 2018 Bruno Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
